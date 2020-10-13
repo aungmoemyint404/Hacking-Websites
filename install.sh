bb9740b43ffd5c3556bd92e30c11364b9b99281c
aungmoemy <72740033+aungmoemy@users.noreply.github.com>
2020-10-14 03:30:49 +0630

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+clear
+echo -e '\e[32;m     Wait  Iinstalling Packages\e[0m  '
+sleep 2
+clear
+apt install git -y
+apt install ruby -y
+gem install lolcat
+clear
+echo -e '\e[32;m           Successful Install\e[0m '
+sleep 2
+clear
